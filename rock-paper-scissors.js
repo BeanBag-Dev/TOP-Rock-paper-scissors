6d1bc9948d2594c83c1b2f453776e90e2cb2a832
BeanBag-Dev <253141417+BeanBag-Dev@users.noreply.github.com>
2026-01-11 09:41:08 +0000

    Added the following final touches to the game: - game state tracking - player and computer score tracking - A function to restart the game - A function to check if the game is to be ended - Incrementing of the player and computer scores

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,21 +1,28 @@
 const options = ["🪨 Rock", "📄 Paper", "✂️ Scissors"];
 
+let gameRunning = true;
+let userScore = 0;
+let computerScore = 0;
+
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
+const restartBtn = document.getElementById("restart");
 const resultBar = document.querySelector(".result p");
+
+const playerScoreUI = document.getElementById("player-score");
+const computerScoreUI = document.getElementById("computer-score");
 
 rockBtn.addEventListener("click", () => playGame("🪨 Rock"));
 paperBtn.addEventListener("click", () => playGame("📄 Paper"));
 scissorsBtn.addEventListener("click", () => playGame("✂️ Scissors"));
-
+restartBtn.addEventListener("click", () => restartGame());
 
 function getComputerChoice(){
     return options[Math.floor(Math.random() * options.length)];
 }
 
 function findWinner(userChoice, computerChoice){
-                console.log(userChoice)
     if (userChoice != computerChoice){
         if (userChoice === "🪨 Rock"){
             return computerChoice === "✂️ Scissors" ? "User" : "Computer";
@@ -29,30 +36,53 @@ function findWinner(userChoice, computerChoice){
     return "Draw";
 }
 
-/* TODO: 
+function restartGame(){
+    [userScore, computerScore] = [0, 0];
+    playerScoreUI.textContent = "Player - 0";
+    computerScoreUI.textContent = "0 - Computer";
+    resultBar.style.display = "none";
+    restartBtn.style.display = "none";
+    gameRunning = true;
+}
 
-- Change "User" to "You" in text outputs
-- Add player and computer score tracking
-- Add player and computer score HTML elements
-- Add restart button element
-- Add best of 5 to game over
-- Add a restart game button on game over
-- Add better feedback on the result bar when it changes (so that similar selections can be clearly seen as new messages)
-*/
+function checkGameEnd(){
+    if (userScore == 5 || computerScore == 5){
+        gameRunning = false;
+        restartBtn.style.display = "block";
+        resultBar.textContent = `GAME OVER! ${userScore == 5 ? "You" : "The Computer"} wins!`;
+        resultBar.style.backgroundColor = computerScore == 5 ? "#d35f42" : "#6bd342";
+    }
+}
 
 function playGame(userChoice){
-    const computerChoice = getComputerChoice()
+    if (!gameRunning) return;
 
-    const result = findWinner(userChoice, computerChoice)
+    const computerChoice = getComputerChoice();
+
+    const result = findWinner(userChoice, computerChoice);
 
     if (result === "Draw"){
         resultBar.style.backgroundColor = "#d39942" // Orange
-        resultBar.textContent = `User chose ${userChoice}, Computer chose ${computerChoice} - It's a draw!`;
+        resultBar.textContent = `You chose ${userChoice}, Computer chose ${computerChoice} - It's a draw!`;
         resultBar.style.display = "block";
     } else {
+
+        switch (result){
+            case "Computer":
+                ++computerScore;
+                computerScoreUI.textContent = computerScore + " - Computer";
+                break;
+            case "User":
+                ++userScore;
+                playerScoreUI.textContent = "Player - " + userScore;
+                break;
+        }
+
         resultBar.style.backgroundColor = result === "Computer" ? "#d35f42" : "#6bd342";
-        resultBar.textContent = `User chose ${userChoice}, Computer chose ${computerChoice} - ${result} wins!`;
+        resultBar.textContent = `You chose ${userChoice}, Computer chose ${computerChoice} - ${result} wins!`;
         resultBar.style.display = "block";
+
+        checkGameEnd();
     }
     
 }
